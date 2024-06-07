3868c1b9eeb4a009824c9ebdf0e014a6e0a90ae9
SyedaShafi <syedashafi4@gmail.com>
2024-06-07 16:20:09 +0600

    updated

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ btn.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
 });
 
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function () {
+  loader.style.display = 'none';
+  var mainContent = document.getElementById('main-content');
+  mainContent.classList.remove('hidden');
+});
